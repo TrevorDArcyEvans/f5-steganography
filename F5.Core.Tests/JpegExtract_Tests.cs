42edebd768b6729d165b077e2763c61f508e57a7
Trevor D'Arcy-Evans <trevor.darcyevans@apdcomms.com>
2022-04-04 07:24:33 +0100

    - check embedded string

--- F5.Core.Tests/JpegExtract_Tests.cs
+++ F5.Core.Tests/JpegExtract_Tests.cs
@@ -2,6 +2,7 @@
 
 using System.IO;
 using NUnit.Framework;
+using FluentAssertions;
 
 public sealed class JpegExtract_Tests
 {
@@ -10,6 +11,12 @@ public sealed class JpegExtract_Tests
   {
     using var ms = new MemoryStream();
     using var extractor = new JpegExtract(ms, "abc123");
+
     extractor.Extract(File.OpenRead("borneo-embed.jpg"));
+    ms.Position = 0;
+    var sr = new StreamReader(ms);
+    var data = sr.ReadToEnd();
+
+    data.Should().Be("I Am Groot");
   }
 }
